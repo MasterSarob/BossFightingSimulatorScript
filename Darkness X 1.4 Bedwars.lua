1e5d4414a6c953e4ef19cb9a6317caddf00fe5c9
MasterSarob <107350728+MasterSarob@users.noreply.github.com>
2022-06-27 09:35:13 +0700

    Darkness X 1.4

--- Darkness X 1.4 Bedwars.lua
+++ Darkness X 1.4 Bedwars.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/990623975346421760/990629056825417798/out.lua",true))()
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/990623975346421760/990806663898476584/out.lua",true))()
