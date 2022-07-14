d7ed395f859b5298ae4b37025e18074658ae0bfe
MasterSarob <107350728+MasterSarob@users.noreply.github.com>
2022-07-14 22:18:10 +0700

    Update and rename Darkness X 1.4 Bedwars.lua to BossFightingSimulator.lua

---
+++ BossFightingSimulator.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/997157450253877278/997157522412687462/out.lua"))()
--- Darkness X 1.4 Bedwars.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/990948262225788948/990948330718781480/out.lua",true))()
